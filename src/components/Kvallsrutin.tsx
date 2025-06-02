9ec879fd0e2192c8547e4f9045a2663fae981513
DulamaA <114867084+DulamaA@users.noreply.github.com>
2025-06-02 11:21:11 +0200

    refactor(component): change static routine list into an array of objects

--- src/components/Kvallsrutin.tsx
+++ src/components/Kvallsrutin.tsx
@@ -1,20 +1,25 @@
+
+ export const rutiner = [
+  { id: 1, titel: "Plocka leksaker", klar: false },
+  { id: 2, titel: "Äta middag", klar: false },
+  { id: 3, titel: "Gå på toaletten", klar: false },
+  { id: 4, titel: "Duscha", klar: false },
+  { id: 5, titel: "Borsta tänderna", klar: false },
+  { id: 6, titel: "Ta på pyjamas", klar: false },
+  { id: 7, titel: "Läsa en bok eller lyssna på en saga", klar: false },
+  { id: 8, titel: "Säga godnatt!", klar: false },
+];
+
 export const Kvallsrutin = () => {
   return (
-    <>
-      <div>
-        <h2>Kvällsrutin</h2>
-        <ul>
-          <li>Plocka leksaker</li>
-          <li>Äta middag</li>
-          <li>Gå på toaletten</li>
-          <li>Duscha</li>
-          <li>Borsta tänderna</li>
-          <li>Ta på pyjamas</li>
-          <li>Läsa en bok eller lyssna på en saga</li>
-          <li>Säga godnatt!</li>
-        </ul>
-      </div>
-    </>
+    <div>
+      <h2>Kvällsrutin</h2>
+      <ul>
+        {rutiner.map((punkt) => (
+          <li key={punkt.id}>{punkt.titel}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
