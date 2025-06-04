c8ae938886a2825121468b221c3a5bb7e0ff79fb
DulamaA <114867084+DulamaA@users.noreply.github.com>
2025-06-04 14:53:54 +0200

    update(tyoes): change 'unknown' to 'void'

--- src/models/TodoTypes.ts
+++ src/models/TodoTypes.ts
@@ -1,17 +1,17 @@
-import { Todo } from './Todo';
+import { Todo } from "./Todo";
 
 export type TodoListProps = {
   todos: Todo[];
-  onToggle: (id: number) => unknown;
-  onDelete: (id: number) => unknown;
+  onToggle: (id: number) => void;
+  onDelete: (id: number) => void;
 };
 
 export type TodoItemProps = {
   todo: Todo;
-  onToggle: (id: number) => unknown;
-  onDelete: (id: number) => unknown;
+  onToggle: (id: number) => void;
+  onDelete: (id: number) => void;
 };
 
 export type TodoFormProps = {
-  onAdd: (titel: string) => unknown;
+  onAdd: (titel: string) => void;
 };
